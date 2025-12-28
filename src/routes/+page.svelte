57565a0e56749c7e3f12ec02e1a706872c97d244
Ethan Marks <87040432+ColourlessSpearmint@users.noreply.github.com>
2025-12-28 18:11:49 -0500

    add basic +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,38 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<header class="header">
+    <h1 class="title">Not New Year Yet*</h1>
+    <div class="subtitle">*99.73% Accurate</div>
+</header>
+
+<div class="countdown">
+    <div class="time-unit">
+        <span class="time-value">??</span>
+        <span class="time-label">Days</span>
+    </div>
+    <div class="time-unit">
+        <span class="time-value">??</span>
+        <span class="time-label">Hours</span>
+    </div>
+    <div class="time-unit">
+        <span class="time-value">??</span>
+        <span class="time-label">Minutes</span>
+    </div>
+    <div class="time-unit">
+        <span class="time-value">??</span>
+        <span class="time-label">Seconds</span>
+    </div>
+</div>
+
+<noscript>
+    <p id="noscript">Please enable JavaScript for full functionality</p>
+</noscript>
+
+<footer class="footer">
+    <div class="decorative-line"></div>
+    <div class="credits">
+        Created by <a
+            href="https://github.com/ethmarks"
+            target="_blank"
+            rel="noopener noreferrer">ethmarks</a
+        >
+    </div>
+</footer>
